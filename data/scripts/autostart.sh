a4285c7420eea740676fa9a51c07dfda9bd08f1a
Lu Hui <luhux76@gmail.com>
2022-04-14 15:37:59 +0800

    data/scripts/autostart.sh: gtk_im_module 和 qt_im_module 容易出问题，禁止它

--- data/scripts/autostart.sh
+++ data/scripts/autostart.sh
@@ -6,8 +6,8 @@ xset -dpms
 
 # Input Method
 export XMODIFIERS=@im=fcitx
-export GTK_IM_MODULE=fcitx
-export QT_IM_MODULE=fcitx
+export GTK_IM_MODULE=xim
+export QT_IM_MODULE=xim
 fcitx-autostart
 
 # background
